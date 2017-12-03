3470f8f62e35d06a53bbee6b955c7aeeb1a6c78f
Vincent Tencé <vtence@gmail.com>
2017-12-03 15:28:45 -0500

    day 3

---
+++ src/main/kotlin/day3/spiral_memory.kt
@@ -0,0 +1,88 @@
+package day3
+
+import kotlin.coroutines.experimental.buildSequence
+import kotlin.math.abs
+import kotlin.math.max
+
+data class Vector(val x: Int, val y: Int) {
+    private fun bottomRight(): Boolean = x > 0 && x == 1 - y
+
+    private fun topLeft(): Boolean = x < 0 && x == -y
+
+    private fun topRight(): Boolean = x > 0 && x == y
+
+    private fun bottomLeft(): Boolean = x < 0 && x == y
+
+    fun corner(): Boolean = bottomRight() || topRight() || topLeft() || bottomLeft()
+
+    fun length(): Int = abs(x) + abs(y)
+
+    operator fun plus(other: Vector): Vector = Vector(x + other.x, y + other.y)
+
+    override fun toString(): String {
+        return "($x, $y)"
+    }
+}
+
+object Neighbors {
+    val around = arrayOf(
+            Vector(-1, 1), Vector(0, 1), Vector(1, 1),
+            Vector(-1, 0),                      Vector(1, 0),
+            Vector(-1, -1), Vector(0, -1), Vector(1, -1))
+
+
+    fun of(pos: Vector): List<Vector> = around.map { pos + it }
+}
+
+enum class Direction(val v: Vector) {
+    EAST(Vector(1, 0)), NORTH(Vector(0, 1)), WEST(Vector(-1, 0)), SOUTH(Vector(0, -1));
+
+    fun turnLeft(): Direction {
+        if (this == EAST) return NORTH;
+        if (this == NORTH) return WEST;
+        if (this == WEST) return SOUTH;
+        return EAST;
+    }
+
+    fun move(from: Vector): Vector = from + v
+}
+
+object spiral {
+    private fun loop(): Sequence<Vector> {
+        return buildSequence {
+            var pos = Vector(0, 0)
+            var direction = Direction.EAST
+
+            while(true) {
+                yield(pos)
+                if (pos.corner()) direction = direction.turnLeft()
+                pos = direction.move(pos)
+            }
+        }
+    }
+
+    fun coordinates(steps: Int): Vector = loop().take(steps).last()
+
+    fun distance(steps: Int): Int = coordinates(steps).length()
+
+    private fun sumOfAdjacentSquares(startValue: Int): Sequence<Int> {
+        val matrix = mutableMapOf<Vector, Int>()
+
+        return loop().map { coordinates ->
+            val value = Neighbors.of(coordinates).map { matrix[it] ?: 0 }.sumBy { it }
+            matrix.put(coordinates, max(value, startValue))
+            value
+        }
+    }
+
+    fun firstLargerThan(n: Int): Int? = sumOfAdjacentSquares(1).first { it > n }
+}
+
+val spiralInput = 265149
+
+fun main(args: Array<String>) {
+    println("Solution 1: ${spiral.distance(spiralInput)}")
+    println("Solution 2: ${spiral.firstLargerThan(spiralInput)}")
+}
+
+
---
+++ src/test/kotlin/day3/spiral_memory_test.kt
@@ -0,0 +1,57 @@
+package day3
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import org.junit.Test
+
+class SpiralMemoryTest {
+
+    @Test
+    fun `full square looping`() {
+        assertThat(spiral.coordinates(1), equalTo(Vector(0, 0)))
+        assertThat(spiral.coordinates(2), equalTo(Vector(1, 0)))
+        assertThat(spiral.coordinates(3), equalTo(Vector(1, 1)))
+        assertThat(spiral.coordinates(4), equalTo(Vector(0, 1)))
+        assertThat(spiral.coordinates(5), equalTo(Vector(-1, 1)))
+        assertThat(spiral.coordinates(6), equalTo(Vector(-1, 0)))
+        assertThat(spiral.coordinates(7), equalTo(Vector(-1, -1)))
+        assertThat(spiral.coordinates(8), equalTo(Vector(0, -1)))
+        assertThat(spiral.coordinates(9), equalTo(Vector(1, -1)))
+    }
+
+    @Test
+    fun `spiral loops`() {
+        assertThat(spiral.coordinates(1), equalTo(Vector(0, 0)))
+        assertThat(spiral.coordinates(9), equalTo(Vector(1, -1)))
+        assertThat(spiral.coordinates(25), equalTo(Vector(2, -2)))
+        assertThat(spiral.coordinates(49), equalTo(Vector(3, -3)))
+    }
+
+    @Test
+    fun `distance to center`() {
+        assertThat(spiral.distance(1), equalTo(0))
+        assertThat(spiral.distance(12), equalTo(3))
+        assertThat(spiral.distance(23), equalTo(2))
+        assertThat(spiral.distance(1024), equalTo(31))
+    }
+
+    @Test
+    fun `adjacent squares`() {
+        assertThat(Neighbors.of(Vector(1, 1)), equalTo(listOf(
+                Vector(0, 2), Vector( 1, 2), Vector(2, 2),
+                Vector(0, 1),                       Vector(2, 1),
+                Vector(0, 0), Vector( 1, 0), Vector(2, 0))))
+
+    }
+
+    @Test
+    fun `stress values`() {
+        assertThat(spiral.firstLargerThan(1), equalTo(2))
+        assertThat(spiral.firstLargerThan(2), equalTo(4))
+        assertThat(spiral.firstLargerThan(4), equalTo(5))
+        assertThat(spiral.firstLargerThan(11), equalTo(23))
+        assertThat(spiral.firstLargerThan(122), equalTo(133))
+        assertThat(spiral.firstLargerThan(750), equalTo(806))
+    }
+}
+
