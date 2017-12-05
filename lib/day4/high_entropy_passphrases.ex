43c269d911e0d1d225757eaa08c4cf5eaa90073c
Vincent Tencé <vtence@gmail.com>
2017-12-04 21:52:01 -0500

    Day 4, in Elixir

---
+++ lib/day4/high_entropy_passphrases.ex
@@ -0,0 +1,25 @@
+defmodule AdventOfCode2017.HighEntropyPassphrases do
+
+  def count_valid(passphrase_list) do
+    passphrase_list |> String.split("\n") |> Enum.filter(&is_valid/1) |> Enum.count
+  end
+
+  def is_valid(""), do: false
+
+  def is_valid(passphrase) when is_binary(passphrase) do
+    is_valid(String.split(passphrase))
+  end
+
+  def is_valid(passphrase) do
+    !contains_duplicates([], passphrase |> Enum.map(&to_letters/1))
+  end
+
+  defp contains_duplicates(_, []), do: false
+
+  defp contains_duplicates(a, b) do
+    [head | tail] = b
+    head in a or contains_duplicates(a ++ [head], tail)
+  end
+
+  defp to_letters(passphrase), do: to_charlist(passphrase) |> Enum.sort
+end
---
+++ test/day4/high_entropy_passphrases_test.exs
@@ -0,0 +1,23 @@
+defmodule AdventOfCode2017.HighEntropyPassphrases.Test do
+  use ExUnit.Case
+
+  import AdventOfCode2017.HighEntropyPassphrases
+
+  test "rejects empty passphrase" do
+    assert is_valid("") == false
+  end
+
+  test "rejects duplicate words in passphrases" do
+    assert is_valid("aa bb cc dd ee")
+    assert is_valid("aa bb cc dd aa") == false
+    assert is_valid("aa bb cc dd aaa")
+  end
+
+  test "rejects passphrases with words anagrams" do
+    assert is_valid("abcde fghij")
+    assert is_valid("abcde xyz ecdab") == false
+    assert is_valid("a ab abc abd abf abj")
+    assert is_valid("iiii oiii ooii oooi oooo")
+    assert is_valid("oiii ioii iioi iiio") == false
+  end
+end
