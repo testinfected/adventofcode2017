7d07400ac402f58d68fed3149c61c80a3f18c1bf
Vincent Tencé <vtence@gmail.com>
2017-12-23 23:38:52 -0500

    Day 14 - part 2 - in Ruby

--- lib/day14/disk_defragmentation.rb
+++ lib/day14/disk_defragmentation.rb
@@ -1,4 +1,5 @@
 require 'lib/day10/knot_hash'
+require 'set'
 
 def to_bits(hexa)
   hexa.chars.map {|c| "%04b" % c.to_i(16)}.join("")
@@ -8,14 +9,58 @@ def decode(key)
   to_bits(dense_hash_of(key)).chars.map &:to_i
 end
 
-def disk(key)
-  (0...128).map {|row| decode("#{key}-#{row}")}
+Disk = Struct.new(:grid) do
+  def self.decrypt(key)
+    Disk.new((0...128).map {|row| decode("#{key}-#{row}")})
+  end
+
+  def total_used
+    grid.flatten.reduce &:+
+  end
+
+  def neighbors_of(row, col)
+    [[0, -1], [1, 0], [0, 1], [-1, 0]].map {|x, y| [row + x, col + y]}.select {|x, y| valid?(x, y)}
+  end
+
+  def used?(row, col)
+    grid[row][col] == 1
+  end
+
+  def valid?(row, col)
+    row >= 0 and row < grid.size and col >= 0 and col < grid[row].size
+  end
+
+  def region_containing(square)
+    frontier, visited = [square], [square]
+    until frontier.empty?
+      neighbors_of(*frontier.shift).select {|neighbor| used?(*neighbor) and not visited.include?(neighbor)}
+                                   .each {|neighbor| frontier << neighbor and visited << neighbor}
+    end
+    visited.to_set
+  end
+
+  def each_used_square
+    grid.each_index do |row|
+      grid[row].each_index do |col|
+        yield [row, col] if used?(row, col)
+      end
+    end
+  end
+
+  def all_regions
+    regions = []
+    each_used_square do |square|
+      regions << region_containing(square) unless regions.any? {|region| region.include? square}
+    end
+    regions
+  end
+
+  def total_regions
+    all_regions.size
+  end
 end
 
-def used(disk)
-  disk.flatten.reduce &:+
-end
 
-input = "ffayrhll"
-
-puts "Part 1: #{used(disk(input))}"
+disk = Disk.decrypt("ffayrhll")
+puts "Part 1: #{disk.total_used}"
+puts "Part 2: #{disk.total_regions}"
--- test/day14/test_disk_defragmentation.rb
+++ test/day14/test_disk_defragmentation.rb
@@ -3,6 +3,20 @@ require 'test/unit'
 
 class DiskDefragmentationTest < Test::Unit::TestCase
 
+  def setup
+    @disk = disk(<<-EOG
+11.2.3..
+.1.2.3.4
+....5.6.
+7.8.55.9
+.88.5...
+88..5...
+.8......
+88......
+    EOG
+    )
+  end
+
   def test_hexa_to_bits
     assert_equal("1010", to_bits("a"))
     assert_equal("0000", to_bits("0"))
@@ -15,6 +29,30 @@ class DiskDefragmentationTest < Test::Unit::TestCase
   end
 
   def test_counting_used_squares
-      assert_equal(8108, used(disk("flqrgnkx")))
+    assert_equal(8108, Disk.decrypt("flqrgnkx").total_used)
+  end
+
+  def test_neighbors_of_square
+    assert_equal(set([0, 1], [1, 2], [2, 1], [1, 0]), @disk.neighbors_of(1, 1).to_set)
+    assert_equal(set([0, 1], [1, 0]), @disk.neighbors_of(0, 0).to_set)
+  end
+
+  def test_finding_regions
+    assert_equal(set([0, 0], [0, 1], [1, 1]), @disk.region_containing([0, 0]))
+    assert_equal(set([0, 0], [0, 1], [1, 1]), @disk.region_containing([1, 1]))
+    assert_equal(set([0, 3], [1, 3]), @disk.region_containing([1, 3]))
+  end
+
+  def test_counting_regions
+    assert_equal(9, @disk.total_regions)
+    assert_equal(1242, Disk.decrypt("flqrgnkx").total_regions)
+  end
+
+  def disk(grid)
+    Disk.new(grid.split("\n").map {|row| row.chars.map {|square| square == "." ? 0 : 1}})
+  end
+
+  def set(*squares)
+    squares.to_set
   end
 end
